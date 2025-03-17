d04a580d115e8168776db5400bb7fe4edf596142
dydgk79 <dydgk79@naver.com>
2025-03-17 21:10:42 +0900

    이진탐색 풀었다

--- 20250318_DnC/5207.binary_search.py
+++ 20250318_DnC/5207.binary_search.py
@@ -1,43 +1,63 @@
-import sys
-sys.stdin = open("sample_input.txt", "r")
-
-
-def bin_search(x, left, right, middle):
-    global cnt
-    global flag
+def bin_search(l, r, m):
+    global ans
     global is_in
-    if x == N_list[middle]:
+    global flag
+    global cnt
+    if l > r:
+        return -1
+    if N_list[m] == M_list[idx]:
         is_in = True
-        return
-    if left == right:
-        return
-    if x < N_list[middle]:
-        cnt += 1
-        if flag == 'right':
-            cnt = 2
-        flag = 'left'
-        bin_search(x, left, middle-1, (left + middle-1)//2)
-    if x > N_list[middle]:
-        cnt += 1
+        cnt = 2
+        return True
+    elif M_list[idx] < N_list[m]:
         if flag == 'left':
-            cnt = 2
+            flag = False
+            cnt = 0
+            return
+        cnt += 1
+        flag = 'left'
+        return bin_search(l, m-1, (l+m-1)//2)
+    elif M_list[idx] > N_list[m]:
+        if flag == 'right':
+            flag = False
+            cnt = 0
+            return
+        cnt += 1
         flag = 'right'
-        bin_search(x, middle+1, right, (middle+1 + right)//2)
+        return bin_search(m+1, r, (m+1+r)//2)
 
 
 T = int(input())
 
-for tc in range(1, T+1):
+for tc in range(1, 1+T):
     N, M = map(int, input().split())
     N_list = list(map(int, input().split()))
-    M_list = list(map(int, input().split()))
     N_list.sort()
+    M_list = list(map(int, input().split()))
+
     ans = 0
-    for idx in range(M):
+    left = 0
+    right = N - 1
+    middle = (left + right) // 2
+    for idx in range(len(M_list)):
         is_in = False
         flag = False
         cnt = 0
-        bin_search(M_list[idx], 0, N-1, N//2)
-        if cnt >= 2 or is_in:
+        bin_search(left, right, middle)
+        if cnt >= 2 and is_in:
             ans += 1
     print(f'#{tc} {ans}')
+
+"""
+3
+3 3
+1 2 3
+2 3 4
+3 5
+1 3 5
+2 4 6 8 10
+5 5
+1 3 5 7 9
+1 2 3 4 5
+
+"""
